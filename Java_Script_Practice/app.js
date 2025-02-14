128eb980e14e64e518e7d60beb7ae5d1bd3b631f
Tiwalola Tejumola <tiwatej@gmail.com>
2025-02-14 12:27:19 +0100

    Created Car Dealership Feedback App project

---
+++ Java_Script_Practice/app.js
@@ -0,0 +1,2 @@
+import { greet} from './hello.js';
+greet(); // Hello, World!
---
+++ Java_Script_Practice/hello.js
@@ -0,0 +1,5 @@
+function greet() {
+    console.log("Hello, World!");
+}
+
+export { greet }; // Export the greet function
